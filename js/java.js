fe24ee629b10782d2039fbbe3e067b78a3127487
Baouz <51371093+Baouz@users.noreply.github.com>
2019-07-10 19:30:34 +0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,12 @@
+// Animation en croix burger //
+$(".burger-menu").click(function () {
+    $(this).toggleClass("menu-on");
+});
+
+
+// texte de la nav qui va et viens //
+$(".burger-menu").click(function () {
+    $(".hidden").slideToggle("show");
+});
+
+
